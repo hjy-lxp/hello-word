d40defde31c46c95743de9fa47d0ecbd59fd257b
hjy-lxp <513410507@qq.com>
2021-02-04 23:53:46 +0800

    第一次进行数据提交！

---
+++ Moudle01/src/test/Person.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author hjystart
+ * @create 2021-02-04 23:51
+ */
+public class Person {
+    public static void main(String[] args) {
+        String sex="男";
+        System.out.println(""+sex);
+    }
+}
