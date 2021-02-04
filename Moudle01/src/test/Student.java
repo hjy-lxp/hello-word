c2d7ebf9536246f51a3c7236f09db58c2860db62
hjy-lxp <513410507@qq.com>
2021-02-04 22:46:55 +0800

    第一次进行数据提交！

---
+++ Moudle01/src/test/Student.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author hjystart
+ * @create 2021-02-04 22:43
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("你好，狗子哥！");
+    }
+}
